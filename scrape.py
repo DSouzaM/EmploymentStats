d5f4a2b5c8236d248ccd71c64d3b3d1dd3873959
Matt D'Souza <matt.dsouza@gmail.com>
2016-05-28 22:11:40 -0400

    Dynamic date retrieval

--- scrape.py
+++ scrape.py
@@ -1,11 +1,10 @@
 import requests
 import re
 
-index_url = 'https://info.uwaterloo.ca/infocecs/students/statistics/index.php'
-parameterized_url = 'https://info.uwaterloo.ca/infocecs/students/statistics/graph.php?Term={0}&Faculty={1}&Level={2}&Date={3}'
-regex_string = '<p>.+Employed Students = (\d+)[\S\s]+?Unemployed Students = (\d+)[\S\s]+?width="25px">(.+)<'
-
-
+dates_url = 'https://info.uwaterloo.ca/infocecs/students/statistics/index.php'
+dates_regex = '&Date=([0-9]{8})'
+stats_url = 'https://info.uwaterloo.ca/infocecs/students/statistics/graph.php?Term={0}&Faculty={1}&Level={2}&Date={3}'
+stats_regex = '<p>.+Employed Students = (\d+)[\S\s]+?Unemployed Students = (\d+)[\S\s]+?width="25px">(.+)<'
 
 def get_user():
 	with open('auth') as auth:
@@ -20,29 +19,27 @@ def get_auth():
 	with open('auth') as auth:
 		return auth.read().splitlines()
 
-def parse(results): 
-	parsed = []
-	for entry in results:
-		obj = {}
-		obj['employed'] = str(entry[0])
-		obj['unemployed'] = str(entry[1])
-		obj['faculty'] = str(entry[2])
-		parsed.append(obj)
-	return parsed	
+def parse(result): 
+	return {
+	'employed' : str(result[0]),
+	'unemployed' : str(result[1]),
+	'faculty' : str(result[2])
+	}
 
-def query(faculty=80, date=20160421, level=-1, term=1165, username=get_user(), password=get_password()):
-	request = requests.get(parameterized_url.format(term, faculty, level, date), auth=(username, password))
-	results = re.findall(regex_string, request.text)
-	return parse(results)
+def getStats(faculty=80, date=20160519, level=-1, term=1165, username=get_user(), password=get_password()):
+	request = requests.get(stats_url.format(term, faculty, level, date), auth=(username, password))
+	results = re.findall(stats_regex, request.text)
+	return map(parse, results)
 
+def getDatesByTerm(term=1165, username=get_user(), password=get_password()):
+	request = requests.post(dates_url, data = {'Term':term,'Faculty':'80','Level':'-1'}, auth=(username, password))
+	results = re.findall(dates_regex, request.text)
+	return map(str, results)
 
-
-def full_lookup(faculty=80):
-	stats = {}
-	with open('dates') as dates:
-		for date in dates.read().splitlines():
-			stats[date] = query(80,date)
-			sleep(0.1)
+def getFacultyStats(faculty=80, term=1165):
+	stats = []
+	dates = getDatesByTerm(term);
+	for date in dates:
+		statsByDate = {date : getStats(faculty, date, -1, term)}
+		stats.append(statsByDate)
 	return stats
-
-# tentative regex '<p>.+Employed Students = (\d+)[\S\s]+?Unemployed Students = (\d+)[\S\s]+?width="25px">(.+)<'
